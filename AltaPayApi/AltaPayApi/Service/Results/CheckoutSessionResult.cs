175aa72a839ceb5fb29857f6556264cef8f879a3
Shahbaz <shahbaz.mehar@outlook.com>
2026-05-04 17:25:38 +0500

    Add CheckoutSession API method and related classes for checkout session handling

--- AltaPayApi/AltaPayApi/Service/Results/CheckoutSessionResult.cs
+++ AltaPayApi/AltaPayApi/Service/Results/CheckoutSessionResult.cs
@@ -5,10 +5,6 @@ namespace AltaPay.Service
 {
     public class CheckoutSessionResult : ApiResult
     {
-        public string Url { get; set; }
-        public string DynamicJavascriptUrl { get; set; }
-        public string AppUrl { get; set; }
-        public string PaymentRequestId { get; set; }
         public string SessionId { get; set; }
         public string SessionStatus { get; set; }
 
@@ -22,12 +18,7 @@ namespace AltaPay.Service
                 if (!String.IsNullOrEmpty(apiResponse.Body.Result))
                     Result = (Result)Enum.Parse(typeof(Result), apiResponse.Body.Result);
                 else
-                    Result = Result.Success; // Fallback for when Result node is missing (like in checkoutSession)
-
-                Url = apiResponse.Body.Url;
-                DynamicJavascriptUrl = apiResponse.Body.DynamicJavascriptUrl;
-                AppUrl = apiResponse.Body.AppUrl;
-                PaymentRequestId = apiResponse.Body.PaymentRequestId;
+                    Result = Result.Success; // Fallback for when Result node is missing
 
                 if (apiResponse.Body.Session != null)
                 {
